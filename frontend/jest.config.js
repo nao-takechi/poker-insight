94393faf503145421bec70dae607d66d59257bab
Nao Takechi <nao.takechi@gmail.com>
2025-11-20 23:54:21 +0900

    fix: setupTests

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -13,7 +13,7 @@ const customJestConfig = {
     "^next/navigation$": "<rootDir>/__mocks__/next/navigation.ts",
   },
 
-  setupFilesAfterEnv: ["<rootDir>/app/setupTests.ts"],
+  setupFilesAfterEnv: ["<rootDir>/setupTests.ts"],
 
   testPathIgnorePatterns: ["<rootDir>/e2e/", "<rootDir>/node_modules/"],
 
