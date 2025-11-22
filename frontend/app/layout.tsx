6ae1f64a5bde5154f33ebb66a4da56eb7fb6d247
Nao Takechi <nao.takechi@gmail.com>
2025-11-22 23:58:12 +0900

    feat: fadeIn animation

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,5 +1,9 @@
+"use client";
+
 import { BottomNav } from "@/components/BottomNav";
+import { motion } from "framer-motion";
 import { Afacad } from "next/font/google";
+import { usePathname } from "next/navigation";
 import type { ReactNode } from "react";
 import "./globals.css";
 
@@ -14,11 +18,27 @@ const afacad = Afacad({
 });
 
 export default function RootLayout({ children }: RootLayoutProps) {
+  const pathname = usePathname();
+
   return (
     <html lang="ja" className={afacad.variable}>
       <body className="bg-[#f8f7f4]">
         <main className="max-w-[600px] m-auto relative min-h-screen px-6 pt-8 pb-12">
-          {children}
+          <motion.div
+            key={pathname}
+            initial={{ opacity: 0, x: 40 }}
+            animate={{
+              opacity: [0, 0.6, 1],
+              x: [40, 10, 0],
+            }}
+            transition={{
+              duration: 0.48, // ← ほんの少し短く
+              ease: ["easeOut", "easeOut", [0.33, 0.0, 0.2, 1.0]], // ← 初速を軽く強め
+            }}
+          >
+            {children}
+          </motion.div>
+
           <BottomNav />
         </main>
       </body>
